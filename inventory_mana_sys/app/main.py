4ff2247bd6c0763d432b6f443e064c72a7a4b4eb
kavyansh-11 <kavyanshgoswami1109@gmail.com>
2026-06-03 17:53:08 +0530

    cors

--- inventory_mana_sys/app/main.py
+++ inventory_mana_sys/app/main.py
@@ -14,8 +14,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
+        "https://inventory-management-system-553a.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
